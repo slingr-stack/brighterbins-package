4c8e841e7737fc7cb9144f5ec1ddf0ecb5a079b5
Juan Manuel Espinosa <espinosajuanmanuel@gmail.com>
2024-10-08 15:03:33 -0300

    Initial Setup

--- configurationBuilder.js
+++ configurationBuilder.js
@@ -6,13 +6,5 @@
  ****************************************************/
 
 let configurationBuilder = function (config) {
-    config.oauth = {
-        id: 'installationInfo-Skeleton-User-'+sys.context.getCurrentUserRecord().id(),
-        authUrl: "https://example.com/login/oauth/authorize",
-        accessTokenUrl: "https://example.com/login/oauth/access_token",
-        clientId: config.choice,
-        clientSecret: config.text,
-        oauthCallback: config.SKELETON_API_BASE_URL
-    }
     return config;
 }

689b4753550d48729fd72359b5bf15efcde4ea14
Juan Manuel Espinosa <espinosajuanmanuel@gmail.com>
2024-10-13 11:11:56 -0300

    Fixed main api reference

--- scripts/vision.js
+++ scripts/vision.js
@@ -2,14 +2,16 @@
  Vision API
  ****************************************************/
 
+ const api = pkg.brighterbins.api;
+
 exports.list = function () {
-    return pkg.brighterbins.get('/vision/list');
+    return api.get('/vision/list');
 } 
 
 exports.timeseries = function (id, from, to, pageNumber, pageSize) {
     pageNumber = pageNumber || 1;
     pageSize = pageSize || 10;
-    return pkg.brighterbins.post('/vision/timeseries/v2', {
+    return api.post('/vision/timeseries/v2', {
         body: {
             id: id,
             from: from,
